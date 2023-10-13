a0fe9a7324f9efd5aa7e0ef8535a2bb72f5453f2
clo901 <93842458+clo901@users.noreply.github.com>
2023-10-13 15:45:15 -0700

    Create sleep.c
    Updated Sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+  {
+    printf("usage: sleep ticks\n");
+    exit();
+  }
+
+  int ticks;
+  ticks = atoi(argv[1]);
+
+  sleep(ticks);
+  exit();
+}

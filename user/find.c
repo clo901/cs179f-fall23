6252323e41b3c92009449f2d3d4ccb144a2f2628
clo901 <93842458+clo901@users.noreply.github.com>
2023-10-13 19:41:34 -0700

    fmtname Update

--- user/find.c
+++ user/find.c
@@ -11,16 +11,19 @@ fmtname(char *path)
 
   // Find first character after last slash.
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++;
-
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-  return buf;
+       ;
+         p++;
+ 
+   // Return blank-padded name.
+   // if(strlen(p) >= DIRSIZ)
+   // return p;
+   // memmove(buf, p, strlen(p));
+   // memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+   // return buf;
+   
+   return p;
 }
+
 
 void
 ls(char *path)

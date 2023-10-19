f9b6ccf33ffb320234671966bc8242bf2de16132
clo901 <93842458+clo901@users.noreply.github.com>
2023-10-18 22:08:31 -0700

    Update sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -2,27 +2,42 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int i, n;
 
-  if(argc < 2){
+  // Check if the user provided the required argument
+  if(argc < 2)
+  { 
+    //Prints amount of sleep ticks
     printf("Usage: sleep <number of ticks>...\n");
+
+    //Exit program
     exit();
   }
 
+  // Loop through each argument provided by the user
   for(i = 1; i < argc; i++)
   {
+    // Convert the argument to an integer
     n = atoi(argv[i]);
+
+    // Print a message indicating how long the program will sleep     
     printf("sleep: sleeping for %d ticks\n", n);
+    
+    // Sleep for the specified number of ticks
     sleep(n);
-   
+
+    // Attempt to delete the file specified by the argument 
     if(unlink(argv[i]) < 0)
     {
+     // If the file could not be deleted, print an error message and exit the loop
       printf("rm: %s failed to delete\n", argv[i]);
+
       break;
     }
   }
+
+  //Exit program
   exit();
 }

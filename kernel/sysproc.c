480f376f937e78835de0268d2876137f237b8f30
clo901 <93842458+clo901@users.noreply.github.com>
2023-11-02 15:11:08 -0700

    Update sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -40,15 +40,23 @@ sys_wait(void)
 
 uint64
 sys_sbrk(void)
-{
+{ 
   int addr;
   int n;
-
+  
   if(argint(0, &n) < 0)
+  { 
     return -1;
+  }
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  myproc()->sz+=n;
+ if(n < 0)
+{       
+        uvmdealloc(myproc()->pagetable, addr, myproc()->sz);
+}
+
+// if(growproc(n) < 0)
+  //  return -1;
   return addr;
 }
 
